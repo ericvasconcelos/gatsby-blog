6178ae00997647f754aeaf2af4543ff2f7be2162
Eric Vasconcelos de Oliveira <eric.oliveira@printi.com.br>
2021-07-07 22:39:49 -0300

    Finished Gatsby Tutorial Part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "",
+        trackingId: "UA-40075822-3",
       },
     },
     "gatsby-plugin-react-helmet",
