d0e399e6ad9920878734dd2a0d5cec58d7c50dda
Eric Vasconcelos de Oliveira <eric.oliveira@printi.com.br>
2021-07-20 23:12:17 -0300

    Render all infos of country

--- src/components/country/index.js
+++ src/components/country/index.js
@@ -1,51 +1,170 @@
 import React, { memo } from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
-import { heading } from './country.module.scss';
+import Header from '../header';
+import Footer from '../footer';
+import { container, heading, info } from './country.module.scss';
 
-// {
-//   pageContext: {
-//     topLevelDomain: [ '.br' ],
-//     alpha2Code: 'BR',
-//     alpha3Code: 'BRA',
-//     callingCodes: [ '55' ],
-//     altSpellings: [
-//       'BR',
-//       'Brasil',
-//       'Federative Republic of Brazil',
-//       'República Federativa do Brasil'
-//     ],
-//     latlng: [ -10, -55 ],
-//     demonym: 'Brazilian',
-//     area: 8515767,
-//     gini: 54.7,
-//     timezones: [ 'UTC-05:00', 'UTC-04:00', 'UTC-03:00', 'UTC-02:00' ],
-//     borders: [
-//       'ARG', 'BOL', 'COL',
-//       'GUF', 'GUY', 'PRY',
-//       'PER', 'SUR', 'URY',
-//       'VEN'
-//     ],
-//     numericCode: '076',
-//     currencies: [ [Object] ],
-//     languages: [ [Object] ],
-//     translations: {
-//       de: 'Brasilien',
-//       es: 'Brasil',
-//       fr: 'Brésil',
-//       ja: 'ブラジル',
-//       it: 'Brasile',
-//       br: 'Brasil',
-//       pt: 'Brasil',
-//       nl: 'Brazilië',
-//       hr: 'Brazil',
-//       fa: 'برزیل'
-//     },
-//     regionalBlocs: [ [Object] ],
-//     cioc: 'BRA'
-//   },
+const returnning = (val) => val;
+
+const join = (val) => val.join(', ');
+
+const quantity = (val, measure = '') => {
+  const formatted = new Intl.NumberFormat('en-US').format(val);
+  return `${formatted} ${measure}`;
+};
+
+const translation = (val) => (
+  <ul>
+    {Object.entries(val).map(([key, value]) => (
+      <li key={key}>
+        {key.toUpperCase()}: {value}
+      </li>
+    ))}
+  </ul>
+);
+
+const languages = (val) => {
+  return val.reduce((acc, curr) => {
+    if (acc) {
+      return `${acc}, ${curr.name} (${curr.nativeName})`;
+    }
+
+    return `${curr.name} (${curr.nativeName})`;
+  }, '');
+};
+
+const currencies = (val) => {
+  return val.reduce((acc, curr) => {
+    if (acc) {
+      return `${acc} | ${curr.name} (${curr.code} - ${curr.symbol})`;
+    }
+
+    return `${curr.name} (${curr.code} - ${curr.symbol})`;
+  }, '');
+};
+
+const regionalBlocs = (val) => {
+  return val.reduce((acc, curr) => {
+    if (acc) {
+      return `${acc}, ${curr.acronym} (${curr.name})`;
+    }
+
+    return `${curr.acronym} (${curr.name})`;
+  }, '');
+};
+
+const order = [
+  {
+    key: 'capital',
+    name: 'Capital',
+    resolver: returnning,
+  },
+  {
+    key: 'languages',
+    name: 'Languages',
+    resolver: languages,
+  },
+  {
+    key: 'population',
+    name: 'Population',
+    resolver: quantity,
+  },
+  {
+    key: 'gini',
+    name: 'Gini',
+    resolver: returnning,
+  },
+  {
+    key: 'currencies',
+    name: 'Currencies',
+    resolver: currencies,
+  },
+  {
+    key: 'region',
+    name: 'Region',
+    resolver: returnning,
+  },
+  {
+    key: 'subregion',
+    name: 'Subregion',
+    resolver: returnning,
+  },
+  {
+    key: 'area',
+    name: 'Area',
+    resolver: (val) => quantity(val, 'm²'),
+  },
+  {
+    key: 'latlng',
+    name: 'Latitude and longitude',
+    resolver: join,
+  },
+  {
+    key: 'borders',
+    name: 'Borders',
+    resolver: join,
+  },
+  {
+    key: 'regionalBlocs',
+    name: 'Regional blocs',
+    resolver: regionalBlocs,
+  },
+  {
+    key: 'timezones',
+    name: 'Timezones',
+    resolver: join,
+  },
+  {
+    key: 'numericCode',
+    name: 'Numeric code',
+    resolver: returnning,
+  },
+  {
+    key: 'translations',
+    name: 'Translations of name',
+    resolver: translation,
+  },
+  {
+    key: 'callingCodes',
+    name: 'Calling codes',
+    resolver: join,
+  },
+  {
+    key: 'topLevelDomain',
+    name: 'Top level domain',
+    resolver: join,
+  },
+  {
+    key: 'cioc',
+    name: 'CIOC',
+    resolver: returnning,
+  },
+  {
+    key: 'alpha2Code',
+    name: 'Alpha 2 code',
+    resolver: returnning,
+  },
+  {
+    key: 'alpha3Code',
+    name: 'Alpha 3 code',
+    resolver: returnning,
+  },
+  {
+    key: 'altSpellings',
+    name: 'Alternative spellings',
+    resolver: join,
+  },
+  {
+    key: 'demonym',
+    name: 'Demonym',
+    resolver: returnning,
+  },
+];
 
 const CountryLayout = ({ pageContext }) => {
+  const { name, nativeName, flag, url, altSpellings } = pageContext;
+
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -56,18 +175,6 @@ const CountryLayout = ({ pageContext }) => {
       }
     }
   `);
-
-  const {
-    name,
-    nativeName,
-    flag,
-    url,
-    region,
-    subregion,
-    population,
-    capital,
-    altSpellings,
-  } = pageContext;
 
   return (
     <>
@@ -80,27 +187,26 @@ const CountryLayout = ({ pageContext }) => {
         <link rel="canonical" href={data.site.siteMetadata.siteUrl + url} />
       </Helmet>
 
-      <h1 className={heading}>
-        {name} - {nativeName}
-      </h1>
-      <img src={flag} alt={name} />
-      <p>
-        <b>Region: </b>
-        {region}
-      </p>
-      <p>
-        <b>Subregion: </b>
-        {subregion}
-      </p>
-      <p>
-        <b>Population: </b>
-        {new Intl.NumberFormat('en-US').format(population)}
-      </p>
-      <p>
-        <b>Capital: </b>
-        {capital}
-      </p>
+      <Header />
+
+      <div className={container}>
+        <img src={flag} alt={name} />
+
+        <h1 className={heading}>
+          {name} - {nativeName}
+        </h1>
+
+        {order.map(({ key, name, resolver }) => (
+          <div className={info} key={key}>
+            <b>{name}: </b>
+            {resolver(pageContext[key])}
+          </div>
+        ))}
+      </div>
+
+      <Footer />
     </>
   );
 };
+
 export default memo(CountryLayout);
